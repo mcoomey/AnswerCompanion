143e6456096efcb96e820ce99c425999324b292e
Michael Coomey <mcoomey@gmail.com>
2015-02-27 12:03:43 -0500

    course_asset redirect changes complete

--- app/controllers/course_assets_controller.rb
+++ app/controllers/course_assets_controller.rb
@@ -58,6 +58,8 @@ class CourseAssetsController < ApplicationController
  # GET /course_assets/1/edit
   def edit
     @course_asset = CourseAsset.find(params[:id])
+    @course = Course.find_by_id params[:course_id] if params[:course_id] 
+    @subject = Subject.find_by_id params[:subject_id] if params[:subject_id] 
   end
 
   # PUT /course_assets/1
@@ -65,8 +67,8 @@ class CourseAssetsController < ApplicationController
   def update
     @course_asset = CourseAsset.find(params[:id])
     
-    if params[:commit] != "Cancel"
-      if @course_asset.update_attributes(params[:course_asset])
+    if params[:commit] == "Submit"
+      if @course_asset.update_attributes(params[:course_asset].except(:course_id).except(:subject_id))
         @ujsNotice = "Course asset was successfully updated."
         @ujsAlert = nil
         render "update"
@@ -75,6 +77,10 @@ class CourseAssetsController < ApplicationController
         @ujsAlert = @course_asset.errors.full_messages.first
         render "edit"
       end
+      
+    elsif params[:commit] == "Delete"
+      destroy
+      
     else
       @ujsNotice = "Update Course Asset action was canceled."
       render "cancel"
@@ -83,34 +89,64 @@ class CourseAssetsController < ApplicationController
 
   # DELETE /course_assets/1
   def destroy
+    
+    #find and destroy the targetted CourseAsset
     @course_asset = CourseAsset.find(params[:id])
-    @assetable = @course_asset.assetable
-    if @assetable.class.to_s == "Course"
-      @query_string = {:course_id => @assetable.id}
-    else
-      @query_string = {:subject_id => @assetable.id}
-    end
     @course_asset.destroy
-
+      
+    @ujsAlert = nil
+    @ujsNotice = "Course Asset has been deleted."
+    
     ref_url = request.referrer.to_s
     orig_url = request.original_url.to_s
     
     # if user is deleting the currently selected asset
     if (ref_url.index orig_url) == 0
-      # if there are any other assets associated with the course then render the first one
-      @course_asset = @assetable.course_assets.try(:first)
-      if @course_asset
-        new_path = send("course_asset_#{CourseAssetModelType.find_by_id(@course_asset.model_type).name_of_model}_path", @course_asset, @query_string)
-        render :js => "window.location.href = '#{new_path}'"
+
+      # if a course is loaded (i.e. Instructor view)
+      if params[:course_asset][:course_id]
+        @course = Course.find_by_id(params[:course_asset][:course_id])
+        @query_string = {:course_id => @course.id}
+        @course_assets = @course.course_assets.order(:position)
+        @course_asset = @course_assets.try(:first)
         
-        # otherwise just render the course
-      else
-        render :js => "window.location.href = '#{course_path(@assetable, @query_string)}'"
+        # if the course has other assets then redirect to the first one
+        if @course_asset
+          new_path = send("course_asset_#{CourseAssetModelType.find_by_id(@course_asset.model_type).name_of_model}_path", @course_asset, @query_string)
+          render js: "window.location.href = '#{new_path}'"
+          
+        # otherwise redirect to the course show view
+        else
+          render js: "window.location.href = '#{course_path(@course, @query_string)}'"
+          
+        end
+        
+      # else if a subject is loaded (i.e. Student view)
+      elsif params[:course_asset][:subject_id]
+        @subject = Subject.find_by_id(params[:course_asset][:subject_id])
+        @query_string = {:subject_id => @subject.id}
+        @course_assets = @subject.course_assets.try(:sort_by, &:position)
+        @course_asset = @subject.course_assets.try(:first)
+        @enrolled_assets = @subject.enrollment.try(:course).try(:course_assets).try(:sort_by, &:position)
+        @enrolled_asset = @enrolled_assets.try(:first)
+        
+        # if the subject has either another asset (course or enrolled) then redirect there
+        if @enrolled_asset
+          new_path = send("course_asset_#{CourseAssetModelType.find_by_id(@enrolled_asset.model_type).name_of_model}_path", @enrolled_asset, @query_string)
+          render js: "window.location.href = '#{new_path}'"
+        elsif @course_asset
+          new_path = send("course_asset_#{CourseAssetModelType.find_by_id(@course_asset.model_type).name_of_model}_path", @course_asset, @query_string)
+          render js: "window.location.href = '#{new_path}'"
+          
+        # otherwise render the subject show view
+        else
+          render js: "window.location.href = '#{subject_path(@subject, @query_string)}'"
+        end
+        
       end
     else
-      @ujsAlert = nil
-      @ujsNotice = "Course Asset has been deleted."
-    end 
+      render "destroy"
+    end
   end
   
   def sort
